1fe4d2d724d5795812cc10a1dba264ac92fa8f00
Gastón <garabinovich@gmail.com>
2023-11-15 17:36:11 -0300

    Add working day folder, edit svgs from icons and create Navbar component

---
+++ src/app/working-day/page.tsx
@@ -0,0 +1,15 @@
+import { Navbar } from "@/components/Navbar";
+
+export default function WorkingDay() {
+  return (
+    <div className="bg-[#AEE3EF] h-screen">
+        <Navbar/>
+        <div className="flex justify-center">
+            <div className="bg-white p-4 w-80 h-48 m-2.5">
+        
+            </div>
+        </div>
+
+    </div>
+  );
+}
