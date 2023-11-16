049e178d90eb98118cba825711df1d8343794c49
Isidro-Molina <isidromolina260@gmail.com>
2023-11-16 20:14:37 -0300

    get packages view

---
+++ src/app/get-packages/page.tsx
@@ -0,0 +1,83 @@
+import { CheckboxCheck, CheckboxEmpty, LeftArrowIcon } from '@/commons/icons';
+import { Navbar } from '@/components/Navbar';
+
+export default function GetPackages() {
+    return (
+        <main className="bg-[#AEE3EF] h-screen">
+            <Navbar />
+            <section className="flex items-center flex-col h-[82%] mt-7">
+                <div className="bg-[#55BBD1] rounded-lg p-4 w-80 h-[80px] text-white flex flex-wrap justify-between shadow-md">
+                    <LeftArrowIcon className="w-8 ml-[3px] h-fit" />
+                    <p className="mr-[70px] h-[32px] flex items-center">Obtener Paquetes</p>
+                </div>
+                <div className="bg-white rounded-lg p-4 w-80 mt-[-20px] h-[600px] flex flex-col shadow-md">
+                    <p className="text-[#55BBD1] text-center text-xs">¿Cuántos paquetes repartirás hoy?</p>
+                    <p className="mt-2 border-t border-1 border-dashed border-black"></p>
+
+                    <div className="border border-solid border-black h-[60px] rounded-xl mt-4 flex items-center flex-row">
+                        <CheckboxCheck className="w-6 ml-4 h-fit fill-[#AEE3EF]" />
+                        <p className="ml-3 border-dotted border-l border-black h-12 w-1"></p>
+                        <div className="ml-4 text-xs">
+                            <p>Amenabar 2100,</p>
+                            <p>CABA</p>
+                        </div>
+                    </div>
+                    <div className="border border-solid border-black h-[60px] rounded-xl mt-4 flex items-center flex-row">
+                        <CheckboxCheck className="w-6 ml-4 h-fit fill-[#AEE3EF]" />
+                        <p className="ml-3 border-dotted border-l border-black h-12 w-1"></p>
+                        <div className="ml-4 text-xs">
+                            <p>Av Carabobo y Rivadavia,</p>
+                            <p>CABA</p>
+                        </div>
+                    </div>
+                    <div className="border border-solid border-black h-[60px] rounded-xl mt-4 flex items-center flex-row">
+                        <CheckboxCheck className="w-6 ml-4 h-fit fill-[#AEE3EF]" />
+                        <p className="ml-3 border-dotted border-l border-black h-12 w-1"></p>
+                        <div className="ml-4 text-xs">
+                            <p>Melian 1242,</p>
+                            <p>CABA</p>
+                        </div>
+                    </div>
+                    <div className="border border-solid border-black h-[60px] rounded-xl mt-4 flex items-center flex-row">
+                        <CheckboxEmpty className="w-6 ml-4 h-fit" />
+                        <p className="ml-3 border-dotted border-l border-black h-12 w-1"></p>
+                        <div className="ml-4 text-xs">
+                            <p>Castillo 670,</p>
+                            <p>CABA</p>
+                        </div>
+                    </div>
+                    <div className="border border-solid border-black h-[60px] rounded-xl mt-4 flex items-center flex-row">
+                        <CheckboxEmpty className="w-6 ml-4 h-fit" />
+                        <p className="ml-3 border-dotted border-l border-black h-12 w-1"></p>
+                        <div className="ml-4 text-xs">
+                            <p>Gorriti 4595,</p>
+                            <p>CABA</p>
+                        </div>
+                    </div>
+                    <div className="border border-solid border-black h-[60px] rounded-xl mt-4 flex items-center flex-row">
+                        <CheckboxEmpty className="w-6 ml-4 h-fit" />
+                        <p className="ml-3 border-dotted border-l border-black h-12 w-1"></p>
+                        <div className="ml-4 text-xs">
+                            <p>Av. Gral. Mosconi 1056,</p>
+                            <p>CABA</p>
+                        </div>
+                    </div>
+                    <div className="border border-solid border-black h-[60px] rounded-xl mt-4 flex items-center flex-row">
+                        <CheckboxEmpty className="w-6 ml-4 h-fit" />
+                        <p className="ml-3 border-dotted border-l border-black h-12 w-1"></p>
+                        <div className="ml-4 text-xs">
+                            <p>Tacuari 1797,</p>
+                            <p>CABA</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div className="mb-4">
+                <button type="submit" className="w-full px-4 py-2 font-bold bg-[#F4C455] rounded-full">
+                    Iniciar Jornada
+                </button>
+            </div>
+        </main>
+    );
+}
--- src/commons/icons.tsx
+++ src/commons/icons.tsx
@@ -196,18 +196,10 @@ export function CheckboxEmpty({ className = "" }) {
 
 export function CheckboxCheck({ className = "" }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
-    </svg>
+      <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" className={className}>
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
+          <path d="M9 12l2 2l4 -4" />
+      </svg>
   );
 }
