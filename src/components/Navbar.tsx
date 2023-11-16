b7acd1473b0c2008539744640e9c973be4375910
Agustin Sandoval <agustinsandoval1601@gmail.com>
2023-11-16 18:26:30 -0300

    create register view

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+"use client";
+import React from "react";
 import Image from "next/image";
 import pejoton from "../../public/pejoton.png";
 import { LogoutDoorIcon } from "@/commons/icons";
-
+import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
+  const pathName = usePathname();
+
   return (
     <nav className="h-14 bg-[#AEE3EF] shadow-nav py-2 px-8 flex justify-between">
-    <Image src={pejoton} width={56} height={160} alt="pejoton" />
-    <div className="w-7 rounded-md my-[4px] bg-[#55BBD1] shadow-logout-button flex items-center justify-center">
-      <LogoutDoorIcon className="w-6 ml-[3px]" />
-    </div>
-  </nav>
-  )
-}
+      <Image src={pejoton} width={56} height={160} alt="pejoton" />
+      {pathName === "/register" ? (
+        ""
+      ) : (
+        <div className="w-7 rounded-md my-[4px] bg-[#55BBD1] shadow-logout-button flex items-center justify-center">
+          <LogoutDoorIcon className="w-6 ml-[3px]" />
+        </div>
+      )}
+    </nav>
+  );
+};
