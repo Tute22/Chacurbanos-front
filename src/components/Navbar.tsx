a34c2d7e66f06b21b7d52c3783cdd3391025b557
Agustin Sandoval <agustinsandoval1601@gmail.com>
2023-11-24 16:39:01 -0300

    home page is login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import navbarLogo from "../../public/navbarLogo.svg"
+import navbarLogo from "../../public/navbarLogo.svg";
 import { LogoutDoorIcon } from "@/commons/icons/LogoutDoorIcon";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
@@ -9,17 +9,22 @@ import { useRouter } from "next/navigation";
 
 export const Navbar = () => {
   const pathName = usePathname();
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <nav className="h-14 bg-[#AEE3EF] shadow-nav py-2 px-8 flex justify-between">
-      <Link href={'/'}> {/* Despues cambiar a working-day para repartidor y manage-orders para admin */}
-      <Image src={navbarLogo} width={56} height={160} alt="navbarLogo" />
+      <Link href={"/"}>
+        {" "}
+        {/* Despues cambiar a working-day para repartidor y manage-orders para admin */}
+        <Image src={navbarLogo} width={56} height={160} alt="navbarLogo" />
       </Link>
       {pathName === "/register" ? (
         ""
       ) : (
-        <div className="w-7 rounded-md cursor-pointer my-[4px] bg-[#55BBD1] shadow-logout-button flex items-center justify-center" onClick={()=>router.push("/login")}>
+        <div
+          className="w-7 rounded-md cursor-pointer my-[4px] bg-[#55BBD1] shadow-logout-button flex items-center justify-center"
+          onClick={() => router.push("/")}
+        >
           <LogoutDoorIcon className="w-6 ml-[3px]" />
         </div>
       )}
