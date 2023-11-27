c3128b4f83b5c636ef9afe2817e2e8ed59472b3f
martinferrando2005 <ferrandomartin6@gmail.com>
2023-11-27 19:41:42 -0300

    testing husky

--- src/app/distribution/page.tsx
+++ src/app/distribution/page.tsx
@@ -20,11 +20,7 @@ export default function Distribution() {
 
 
 
-
-
-
-
-                                          <main className="mt-8 w-[335px] mx-auto bg-[#55BBD1] text-white   rounded-xl">
+                                                                     <main className="mt-8 w-[335px] mx-auto bg-[#55BBD1] text-white   rounded-xl">
         <div className="flex w-full p-4">
           <Link href={"/working-day"}>
             <LeftArrowIcon className="w-8 ml-[3px] absolute mx-auto" />
