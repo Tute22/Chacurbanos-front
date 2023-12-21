1a7b6d0b8f5e96b7a79ce25df4185aa808a27935
martinferrando2005 <ferrandomartin6@gmail.com>
2023-12-21 16:21:05 -0300

    chau mundo

--- __test__/DeliveryProfile.test.tsx
+++ __test__/DeliveryProfile.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import React from 'react'
-import DeliveryProfile from '@/app/delivery-profile/[id]/[id]'
+import DeliveryProfile from '@/app/delivery-profile/[id]/page'
 
 jest.mock('next/navigation', () => ({
     useRouter() {
